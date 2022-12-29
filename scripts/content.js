e63b378e0051617806af21f75e3a296d477cd856
dahliasan <laninaesloco@gmail.com>
2022-12-30 10:27:47 +1100

    my finished build

---
+++ scripts/content.js
@@ -0,0 +1,48 @@
+const insert = (content) => {
+  // Find Calmly editor input section
+  const element = document.getElementById('main')
+
+  if (!element) return
+
+  // Grab the first p tag so we can replace it with our injection
+
+  const pToRemove = element.childNodes[1]
+  pToRemove?.remove()
+
+  // Split content by \n
+  const splitContent = content.split('\n')
+
+  // Wrap in p tags
+  splitContent.forEach((content) => {
+    const p = document.createElement('p')
+
+    if (content === '') {
+      const br = document.createElement('br')
+      p.appendChild(br)
+    } else {
+      p.textContent = content
+    }
+
+    // Insert into HTML one at a time
+    element.appendChild(p)
+  })
+
+  // On success return true
+  return true
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'inject') {
+    const { content } = request
+
+    // Call this insert function
+    const result = insert(content)
+
+    // If something went wrong, send a failed status
+    if (!result) {
+      sendResponse({ status: 'failed' })
+    }
+
+    sendResponse({ status: 'success' })
+  }
+})
